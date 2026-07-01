e7040fde69aca03ed17f2afa9ab7a3595cdef9b4
TuNguyen229VN <insec229@gmail.com>
2026-07-01 19:31:38 +0700

    feat: Nutrition Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
 import gsap from 'gsap'
 import NavBar from './components/NavBar'
 import HeroSection from './sections/HeroSection'
-import { ScrollTrigger } from 'gsap/all'
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all'
 import MessageSection from './sections/MessageSection';
 import FlavorSection from './sections/FlavorSection';
+import { useGSAP } from '@gsap/react';
+import NutritionSection from './sections/NutritionSection';
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  })
+
+
   return (
     <main>
       <NavBar />
-      <HeroSection />
-      <MessageSection />
-      <FlavorSection />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <NutritionSection/>
+        </div>
+      </div>
     </main>
   )
 }
