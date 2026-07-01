289fa9ab0c995c6ae26e37d1164be8d6b812509f
TuNguyen229VN <insec229@gmail.com>
2026-07-01 20:12:54 +0700

    feat: Testimonial Section

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import FlavorSection from './sections/FlavorSection';
 import { useGSAP } from '@gsap/react';
 import NutritionSection from './sections/NutritionSection';
 import BenefitSection from './sections/BenefitSection';
+import TestimonialSection from './sections/TestimonialSection';
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -30,6 +31,7 @@ const App = () => {
           <FlavorSection />
           <NutritionSection />
           <BenefitSection />
+          <TestimonialSection/>
         </div>
       </div>
     </main>
